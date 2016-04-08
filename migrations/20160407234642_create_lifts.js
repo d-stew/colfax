1542181a6fe8164b657875db5ebae8a76d3fcce6
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-04-07 23:56:25 -0600

    Add knex tables

--- migrations/20160407234642_create_lifts.js
+++ migrations/20160407234642_create_lifts.js
@@ -1,9 +1,11 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('lifts', function (table) {
     table.increments();
-    table.string('username');
-    table.string('first_name').notNullable();
-    table.string('last_name').notNullable();
+    table.date('date').notNullable();
+    table.string('lift');
+    table.integer('reps');
+    table.integer('weight');
+    table.boolean('PR');
   });
 };
 
